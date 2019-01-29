b296afb440d5e63b87f6ce6d0d383b67cfc44cbe
DeltaDizzy <EJBraswell@gmail.com>
2019-01-29 16:49:13 -0600

    update Discord.NET to v2.01

--- source/DeltaBot/DeltaBot/Program.cs
+++ source/DeltaBot/DeltaBot/Program.cs
@@ -8,8 +8,7 @@ namespace DeltaBot
 {
     class Program
     {
-        string filePath = File.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        bool LogExists;
+        string tokenpath = AppDomain.CurrentDomain.BaseDirectory + "tokenbeta.txt";
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
         private CommandHandler _handler; 
@@ -19,9 +18,9 @@ namespace DeltaBot
             client.Log += Log;
             client.MessageReceived += MessageRecieved;
             string token = "";
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "token.txt"))
+            if (File.Exists(tokenpath))
             {
-                token = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "token.txt");
+                token = File.ReadAllText(tokenpath);
             }
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
@@ -32,30 +31,14 @@ namespace DeltaBot
 
         }
         
-        private Task VerifyLogFile()
-        {
-            if(!File.Exists(filePath))
-            {
-                File.Create(filePath + "\DeltaBot.log");
-                Console.WriteLine(Log("Log File Created"));
-                LogExists = true;
-            }
-        }
         private Task Log(LogMessage msg)
         {
-            TextWriter lw = new StreamWriter(filePath + "DeltaBot.log", true);
             Console.WriteLine(msg.ToString());
-            msg.toString().LogWrite(filePath);
             return Task.CompletedTask;
         }
         private async Task MessageRecieved(SocketMessage message)
         {
             
-        }
-        
-        public static LogWrite(this string value, string fileName)
-        {
-            File.WriteAllText(fileName, value);
         }
     }
 }
