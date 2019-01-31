d82cb95bed6df2997a292eae829edbb69fb99081
DeltaDizzy <EJBraswell@gmail.com>
2019-01-31 12:07:28 -0600

    Update Program.cs

--- source/DeltaBot/DeltaBot/Program.cs
+++ source/DeltaBot/DeltaBot/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*
+    Cosmotoad is a Discord bot built in C# using Discord.NET v2.0.1.
+    It is based on DiscordBotBase by foxbot.
+    Terras Ignotas is licensed MIT by KSP-DDR c.2017-2019
+    Turbindrive is licensed MIT by DeltaDizzy and Mrcarrot1 c.2019
+    Ramses System Overhaul is copyrighted by NickRoss120 c.2018-2019, All Rights Reserved
+*/﻿
+
+using System;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -11,108 +19,49 @@ namespace DeltaBot
 {
     class Program
     {
-        private readonly DiscordSocketClient _client;
-        private CommandService _service = new CommandService();
-        private readonly IServiceCollection _map = new ServiceCollection();
-        string tokenpath = AppDomain.CurrentDomain.BaseDirectory + "tokenbeta.tok";
-        public static void Main(string[] args)
-        {
-            new Program().MainAsync().GetAwaiter().GetResult();
-        }
-
-
-        private Program()
-        {
-            _client = new DiscordSocketClient(new DiscordSocketConfig
-            {
-                LogLevel = LogSeverity.Verbose,
-                MessageCacheSize = 50
-            });
-        }
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter.GetResult();
+        
+        private DiscrdSocketClient _client;
+        private IConfiguration _config;
+        
         public async Task MainAsync()
         {
-            _client.Log += Logger;
-            await CommandStart();
-            string token = "";
-            if (File.Exists(tokenpath))
-            {
-                token = File.ReadAllText(tokenpath);
-            }
-            await _client.LoginAsync(TokenType.Bot, token);
+            _client = new DiscordSocketClient();
+            _config = BuildConfig();
+            
+            var services = ConfigureServices();
+            services.GetRequiredService<LogService>();
+            await services.GetRequiedService<CommandHandlingService>().InitializeAsync(services);
+            
+            await _client.LoginAsync(TokenType.Bot _config["token"]);
             await _client.StartAsync();
-            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
-
+            
             await Task.Delay(-1);
-
         }
-
-        IServiceProvider _services;
-
-        private async Task CommandStart()
+        
+        private IServiceProvider ConfigureServices()
         {
-            _services = _map.BuildServiceProvider();
-            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-            _client.MessageReceived += HandleCommandAsync;
+            return new ServiceCollection()
+                //Base Services
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                //Logging
+                .AddLogging()
+                .AddSingleton<LogService>()
+                //Config
+                .AddSingleton(_config)
+                //Add additional services here
+                .BuildServiceProvider();
         }
-
-        private async Task HandleCommandAsync(SocketMessage s)
+        
+        private IConfiguration BuildConfig()
         {
-            var msg = s as SocketUserMessage;
-            if (msg == null)
-            {
-                return;
-            }
-
-            var context = new SocketCommandContext(_client, msg);
-            int argPos = 0;
-            if (msg.HasCharPrefix('&', ref argPos))
-            {
-                var result = await _service.ExecuteAsync(context, argPos, _services);
-
-                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
-                {
-                    await context.Channel.SendMessageAsync(result.ErrorReason);
-                }
-            }
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
-        //TODO: Write one of these yourselves
-        private static Task Logger(LogMessage message)
-        {
-            var cc = Console.ForegroundColor;
-            switch (message.Severity)
-            {
-                case LogSeverity.Critical:
-                case LogSeverity.Error:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case LogSeverity.Warning:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    break;
-                case LogSeverity.Info:
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
-                case LogSeverity.Verbose:
-                case LogSeverity.Debug:
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    break;
-            }
-            Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message}");
-            Console.ForegroundColor = cc;
-
-            // If you get an error saying 'CompletedTask' doesn't exist,
-            // your project is targeting .NET 4.5.2 or lower. You'll need
-            // to adjust your project's target framework to 4.6 or higher
-            // (instructions for this are easily Googled).
-            // If you *need* to run on .NET 4.5 for compat/other reasons,
-            // the alternative is to 'return Task.Delay(0);' instead.
-            return Task.CompletedTask;
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.cosnet")
+                .Build();
         }
     }
 }
