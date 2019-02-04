2f9972e8265ebad1d95cf0e83643424dff6921ca
DeltaDizzy <EJBraswell@gmail.com>
2019-02-04 12:04:21 -0600

    Update Ping.cs

--- [Source]/Cosmotoad/Modules/Ping.cs
+++ [Source]/Cosmotoad/Modules/Ping.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DeltaBot.Modules
+namespace Cosmotoad.Modules
 {
     public class PingModule : ModuleBase<SocketCommandContext>
     {
